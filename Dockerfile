190352ff5c2e82c47616d3d0908732f216ddef19
Bram Crawler <botw44+git@gmail.com>
2022-06-17 09:35:50 +0000

    Updated code to work with new python

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM python:2.7
+FROM python:3.7
 ADD requirements.txt /app/requirements.txt
 ADD ./test_celery/ /app/
 WORKDIR /app/
 RUN pip install -r requirements.txt
-ENTRYPOINT celery -A test_celery worker --concurrency=20 --loglevel=info
+ENTRYPOINT celery -A test_celery worker --concurrency=10 --loglevel=info
--- test_celery/run_tasks.py
+++ test_celery/run_tasks.py
@@ -1,7 +1,13 @@
 from .tasks import longtime_add
 import time
+
 if __name__ == '__main__':
-    url = ['http://example1.com' , 'http://example2.com' , 'http://example3.com' , 'http://example4.com' , 'http://example5.com' , 'http://example6.com' , 'http://example7.com' , 'http://example8.com'] # change them to your ur list.
-    for i in url:
-        result = longtime_add.delay(i)
-        print 'Task result:',result.result
+
+    print('Inside Task Worker')
+
+    with open('files/million.txt', 'r') as source:
+        for line in source:
+            url = 'https://' + line.rstrip()
+            print(url)
+            result = longtime_add.delay(url)
+            print('Task Result: %s', result.result)
